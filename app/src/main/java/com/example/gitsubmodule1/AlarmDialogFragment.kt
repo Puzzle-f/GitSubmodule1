76400da7397a8e20c0250565a82f9ec6b75c3e39
puzzl <puzzleuri@mail.ru>
2024-06-09 22:17:53 +0300

    Создал AlarmDialogFragment, который будет использоваться в качестве субмодуля

---
+++ app/src/main/java/com/example/gitsubmodule1/AlarmDialogFragment.kt
@@ -0,0 +1,31 @@
+package com.example.gitsubmodule1
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class AlarmDialogFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = AlarmDialogFragment()
+    }
+
+    private lateinit var viewModel: AlarmDialogViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_alarm_dialog, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(AlarmDialogViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/gitsubmodule1/AlarmDialogViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.gitsubmodule1
+
+import androidx.lifecycle.ViewModel
+
+class AlarmDialogViewModel : ViewModel() {
+
+}
